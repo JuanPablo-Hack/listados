6e6bcdb333f5cdefd9530f5cffadd3ee28505a89
Ing. Juan Pablo de Jesus Figueroa Jaramillo <programacion@capdam.gob.mx>
2022-04-18 09:15:41 -0500

    Subiendo al repo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  registros = [];
+  constructor(public http: HttpClient) {
+    // alert('Prueba=' + this.cargar());
+    this.cargar();
+  }
+  cargar() {
+    const uri = 'https://www.w3schools.com/angular/customers.php';
+    this.http.get(uri).subscribe((data) => {
+      //process the json data
+      //alert('Datos' + data.records[0].Name);
+      this.registros = data['records'];
+      console.log(data);
+    });
+  }
 }
